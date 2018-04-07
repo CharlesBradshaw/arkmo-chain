9ff48ad941c6aec32e67d4fdd2ce29ea8e814512
Zerthick <zerthick@gmail.com>
2018-04-07 12:39:41 -0400

    Add signature authentication

--- blockchain.py
+++ blockchain.py
@@ -22,31 +22,50 @@ class Blockchain:
         self.unaccepted_payments = set()
         self.finalized_payments = {}
         self.chain = []
+        self.hash_dict = {}
+        self.address_dict = {}
         self.create_genesis_block()
 
     def create_genesis_block(self):
         data = {}
         new_block = Block(data, 0)
         self.chain.append(new_block)
+        self.hash_dict[new_block.hash] = new_block
         return new_block
 
     def create_generic_block(self, data):
+
         new_block = Block(data, self.chain[-1].hash)
         self.chain.append(new_block)
+        self.hash_dict[new_block.hash] = new_block
         return new_block
 
-    def create_request_block(self, from_address, to_address, amount):
+    def create_request_block(self, from_address, to_address, amount, sig, key):
 
         data = {
             'from_address': from_address,
             'to_address': to_address,
             'amount': amount,
         }
-        new_block = self.create_generic_block(data)
-        self.unaccepted_payments.add(new_block.hash)
-        # hash dict
 
-        return new_block
+        if not self.has_stored_key(from_address):
+            if key is not None:
+                if self.validate_address(key, from_address):
+                    self.store_key(from_address, key)
+                    new_block = self.create_generic_block(data)
+                    self.unaccepted_payments.add(new_block.hash)
+                    return new_block
+                else:
+                    raise BlockchainError('Signature is not valid')
+            else:
+                raise BlockchainError('You must provide a public key on your first transaction')
+
+        if self.validate_sig(self.address_dict[from_address], sig):
+            new_block = self.create_generic_block(data)
+            self.unaccepted_payments.add(new_block.hash)
+            return new_block
+        else:
+            raise BlockchainError('Signature not validated')
 
     def finalize_request_block(self, request_block_hash, accepted):
         # todo add lock
@@ -60,10 +79,59 @@ class Blockchain:
             self.finalized_payments[request_block_hash] = new_block.hash
             return new_block
         else:
-            raise Exception('Block Already Finalized', self.finalized_payments[request_block_hash])
+            raise BlockchainError('Block already finalized', self.finalized_payments[request_block_hash])
 
-    def accept_request_block(self, request_block_hash):
-        return self.finalize_request_block(request_block_hash, True)
+    def accept_request_block(self, request_block_hash, sig, key):
 
-    def revoke_request_block(self, request_block_hash):
-        return self.finalize_request_block(request_block_hash, False)
+        transaction_block = self.retrieve_block(request_block_hash)
+        to_address = transaction_block.data['to_address']
+
+        if not self.has_stored_key(to_address):
+            if key is not None:
+                if self.validate_address(key, to_address):
+                    self.store_key(to_address, key)
+                    return self.finalize_request_block(request_block_hash, True)
+                else:
+                    raise BlockchainError('Signature is not valid')
+            else:
+                raise BlockchainError('You must provide a public key on your first transaction')
+
+        if self.validate_sig(self.address_dict[to_address], sig):
+            return self.finalize_request_block(request_block_hash, True)
+        else:
+            raise BlockchainError('Signature not validated')
+
+    def revoke_request_block(self, request_block_hash, sig):
+
+        transaction_block = self.retrieve_block(request_block_hash)
+        from_address = transaction_block.data['from_address']
+
+        if self.has_stored_key(from_address):
+            if self.validate_sig(self.address_dict[from_address], sig):
+                return self.finalize_request_block(request_block_hash, False)
+            else:
+                raise BlockchainError('Signature not validated')
+        else:
+            raise BlockchainError('Public key not available')
+
+    def retrieve_block(self, block_hash):
+        if block_hash in self.hash_dict:
+            return self.hash_dict[block_hash]
+        else:
+            raise BlockchainError('Block does not exist')
+
+    def validate_address(self, key, address):
+        return True
+
+    def validate_sig(self, key, sig):
+        return True
+
+    def has_stored_key(self, address):
+        return address in self.address_dict
+
+    def store_key(self, address, key):
+        self.address_dict[address] = key
+
+
+class BlockchainError(Exception):
+    pass
--- server.py
+++ server.py
@@ -3,7 +3,7 @@ import json
 from flask import Flask, request
 from flask_cors import CORS
 
-from blockchain import Blockchain
+from blockchain import Blockchain, BlockchainError
 
 app = Flask(__name__)
 CORS(app)
@@ -16,8 +16,12 @@ def block_request_create():
     data = request.data.decode("utf-8")
     params = json.loads(data)
 
-    result = block_chain.create_request_block(params['frm'], params['to'], params['amt'])
-    return json.dumps({'new_block': result.__dict__})
+    try:
+        result = block_chain.create_request_block(params['frm'], params['to'], params['amt'], params['sig'],
+                                                  params.get('key', None))
+        return json.dumps({'new_block': result.__dict__})
+    except BlockchainError as e:
+        return str(e), 403
 
 
 @app.route("/blockchain/request/accept", methods=['POST'])
@@ -26,10 +30,10 @@ def block_request_accept():
     params = json.loads(data)
 
     try:
-        result = block_chain.accept_request_block(params['hash'])
+        result = block_chain.accept_request_block(params['hash'], params['sig'], params.get('key', None))
         return json.dumps({'new_block': result.__dict__})
-    except Exception:
-        return 'Block Already Finalized', 401
+    except BlockchainError as e:
+        return str(e), 403
 
 
 @app.route("/blockchain/request/revoke", methods=['POST'])
@@ -38,10 +42,22 @@ def block_request_revoke():
     params = json.loads(data)
 
     try:
-        result = block_chain.revoke_request_block(params['hash'])
+        result = block_chain.revoke_request_block(params['hash'], params['sig'])
         return json.dumps({'new_block': result.__dict__})
-    except Exception:
-        return 'Block Already Finalized', 401
+    except BlockchainError as e:
+        return str(e), 403
+
+
+@app.route("/blockchain/retrieve", methods=['POST'])
+def block_retrieve():
+    data = request.data.decode("utf-8")
+    params = json.loads(data)
+
+    try:
+        result = block_chain.retrieve_block(params['hash'])
+        return json.dumps({'block': result.__dict__})
+    except BlockchainError as e:
+        return str(e), 404
 
 
 if __name__ == "__main__":
